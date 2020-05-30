577ecb6b2c136ea99d3614943062b97866ce7c46
amelieReiss <amelie.reiss@outlook.com>
2020-05-30 09:05:36 +0200

    init atomic styleguide for blog

---
+++ components/atoms/Colors.php
@@ -0,0 +1,4 @@
+<div class="styleguide-box bg--grey-light"></div>
+<div class="styleguide-box bg--blue"></div>
+<div class="styleguide-box bg--orange"></div>
+<div class="styleguide-box bg--black"></div>
