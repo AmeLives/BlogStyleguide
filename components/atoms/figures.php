6b0f6f4069c5705030b04795140a25c7e611a26e
amelieReiss <amelie.reiss@outlook.com>
2020-07-27 18:04:07 +0200

    style(figure, gallery): set styling for images and galleries

---
+++ components/atoms/figures.php
@@ -0,0 +1,151 @@
+<!-- components/atoms/figures.php -->
+<div class="margin__bottom--large">
+    <figure role="group" class="figure">
+      <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      <figcaption class="figure__description">
+        Unter User Experience (UX), das sich als Nutzungserfahrung oder 
+        Nutzungserlebnis übersetzen lässt, versteht man die erlebte und 
+        gefühlte Qualität der Interaktion eines Nutzers insbesondere im 
+        Bereich digitaler Medien.
+      </figcaption>
+    </figure>
+</div>
+
+<div class="margin__bottom--large">
+    <figure role="group" class="gallery" data-cols="2" data-total="2">
+      <figcaption class="gallery__description">
+        Die Entwicklung des SelfHTML-Logos
+      </figcaption>
+      <figure role="group" class="figure">
+        <img class="figure__image"src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>
+    </figure>
+</div>
+
+<div class="margin__bottom--large">
+  <figure role="group" class="gallery" data-cols="3" data-total="2">
+      <figcaption class="gallery__description">
+        Die Entwicklung des SelfHTML-Logos
+      </figcaption>
+      <figure role="group" class="figure">
+        <img class="figure__image"src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>
+    </figure>
+</div>
+
+<div class="margin__bottom--large">
+  <figure role="group" class="gallery" data-cols="3" data-total="5">
+      <figcaption class="gallery__description">
+        Die Entwicklung des SelfHTML-Logos
+      </figcaption>
+      <figure role="group" class="figure">
+        <img class="figure__image"src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>                  
+    </figure>
+</div>
+
+<div class="margin__bottom--large">
+  <figure role="group" class="gallery" data-cols="3" data-total="6">
+      <figcaption class="gallery__description">
+        Die Entwicklung des SelfHTML-Logos
+      </figcaption>
+      <figure role="group" class="figure">
+        <img class="figure__image"src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>  
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+        <figcaption class="figure__description">
+          User Experience ist ein Erfolgsfaktor!
+        </figcaption>
+      </figure>                         
+    </figure>
+</div>
+
+<div class="margin__bottom--large">
+  <figure role="group" class="gallery" data-cols="4" data-total="6">
+      <figure role="group" class="figure">
+        <img class="figure__image"src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>  
+      <figure class="figure" role="group">
+        <img class="figure__image" src="/assets/images/user-experience_placeholder.jpg" alt="SelfHTML-Logo">
+      </figure>                         
+    </figure>
+</div>
---
+++ components/variables/Grid.php
@@ -0,0 +1,1 @@
+<!-- components/variables/Grid.php -->
